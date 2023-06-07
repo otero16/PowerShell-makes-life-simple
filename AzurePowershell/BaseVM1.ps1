920d4a8dda90b0664f80edd0e5595ea32248a400
Juan otero <59911517+otero16@users.noreply.github.com>
2023-06-07 21:13:56 +0200

    Update BaseVM1.ps1

--- AzurePowershell/BaseVM1.ps1
+++ AzurePowershell/BaseVM1.ps1
@@ -1,11 +1,11 @@
 #Install IIS on the server for default website
 Install-WindowsFeature -Name Web-Server -IncludeAllSubFeature -IncludeManagementTools
-#Locate the URL for the psping.exe file
+#Locate the URL for the psping.exe file make sure to test the link before running the script
 $url = "https://live.sysinternals.com/psping.exe"
 $output = "$env:USERPROFILE\psping.exe"
 #Download psping and output the file to the user profile folder to be ready for use
 Invoke-WebRequest -Uri $url -OutFile $output"
-#locate the URL for wireshark download
+#locate the URL for wireshark download, make sure to update the link often
 $url1 = "https://2.na.dl.wireshark.org/win64/Wireshark-win64-4.0.6.exe"
 $output1 = "$env:TEMP\Wireshark-win64-4.0.4.exe"
 # Start the download and save the output file in the temp folder
